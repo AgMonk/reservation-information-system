a00eec1fbca6ea46f0a6ce3e6f933e3dd97a67a2
ginstone <hjg719@139.com>
2022-03-31 16:33:32 +0800

    商户标签 - 过滤条件添加商户类型/地区

--- src/main/java/com/gin/reservationinformationsystem/module/merchant/bo/merchant/Filter4MerchantPo.java
+++ src/main/java/com/gin/reservationinformationsystem/module/merchant/bo/merchant/Filter4MerchantPo.java
@@ -2,6 +2,7 @@ package com.gin.reservationinformationsystem.module.merchant.bo.merchant;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.gin.reservationinformationsystem.sys.request.IFilter;
+import com.gin.reservationinformationsystem.sys.utils.QueryWrapperUtils;
 import com.gin.reservationinformationsystem.sys.utils.StringUtils;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -19,6 +20,12 @@ public class Filter4MerchantPo implements Serializable, IFilter {
     @ApiModelProperty(value = "含有的标签")
     List<String> usedTagsUuid;
 
+    @ApiModelProperty(value = "商户类型UUID")
+    List<String> types;
+
+    @ApiModelProperty(value = "地区")
+    List<String> area;
+
     @Override
     public void handleQueryWrapper(QueryWrapper<?> queryWrapper) {
         if (!StringUtils.isEmpty(usedTagsUuid)) {
@@ -26,5 +33,7 @@ public class Filter4MerchantPo implements Serializable, IFilter {
             final String join = String.join(",", usedTagsUuid);
             queryWrapper.inSql("uuid", String.format(inValue, join));
         }
+        QueryWrapperUtils.inIfNotEmpty(queryWrapper,"type_uuid",types);
+        QueryWrapperUtils.inIfNotEmpty(queryWrapper,"area",area);
     }
 }
