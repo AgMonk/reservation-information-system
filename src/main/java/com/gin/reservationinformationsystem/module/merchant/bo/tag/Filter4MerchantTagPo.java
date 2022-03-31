8785ffb1e38c44a425479d305c1316ce0e111975
ginstone <hjg719@139.com>
2022-03-31 17:14:32 +0800

    商户标签 - 查询添加名称关键字过滤

--- src/main/java/com/gin/reservationinformationsystem/module/merchant/bo/tag/Filter4MerchantTagPo.java
+++ src/main/java/com/gin/reservationinformationsystem/module/merchant/bo/tag/Filter4MerchantTagPo.java
@@ -2,7 +2,9 @@ package com.gin.reservationinformationsystem.module.merchant.bo.tag;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.gin.reservationinformationsystem.sys.request.IFilter;
+import com.gin.reservationinformationsystem.sys.utils.QueryWrapperUtils;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -14,8 +16,11 @@ import java.io.Serializable;
 @Data
 @ApiModel("商户标签过滤条件")
 public class Filter4MerchantTagPo implements Serializable, IFilter {
+    @ApiModelProperty(value = "名称关键字")
+    String name;
+
     @Override
     public void handleQueryWrapper(QueryWrapper<?> queryWrapper) {
-
+        QueryWrapperUtils.likeIfNotEmpty(queryWrapper, "name",name);
     }
 }
