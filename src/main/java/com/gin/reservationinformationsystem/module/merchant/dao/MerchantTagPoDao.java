2170673cc5ac01a5bc132cfd73447dfc6e9598c3
ginstone <hjg719@139.com>
2022-04-15 10:20:50 +0800

    优化 - 商户持有标签和商户公用缓存

--- src/main/java/com/gin/reservationinformationsystem/module/merchant/dao/MerchantTagPoDao.java
+++ src/main/java/com/gin/reservationinformationsystem/module/merchant/dao/MerchantTagPoDao.java
@@ -3,6 +3,7 @@ package com.gin.reservationinformationsystem.module.merchant.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.gin.reservationinformationsystem.module.merchant.entity.MerchantTagPo;
 import org.apache.ibatis.annotations.CacheNamespace;
+import org.apache.ibatis.annotations.CacheNamespaceRef;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,5 +11,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 @CacheNamespace(flushInterval = 5L * 60 * 1000)
+@CacheNamespaceRef(MerchantPoDao.class)
 public interface MerchantTagPoDao extends BaseMapper<MerchantTagPo> {
 }
