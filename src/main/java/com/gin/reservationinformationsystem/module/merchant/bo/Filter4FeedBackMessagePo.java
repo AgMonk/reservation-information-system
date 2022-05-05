c9b08c5e1ac3cefe7cd4389549b8902669f3f5c7
ginstone <hjg719@139.com>
2022-05-05 17:39:58 +0800

    反馈信息 - 修正查询条件

--- src/main/java/com/gin/reservationinformationsystem/module/merchant/bo/Filter4FeedBackMessagePo.java
+++ src/main/java/com/gin/reservationinformationsystem/module/merchant/bo/Filter4FeedBackMessagePo.java
@@ -1,12 +1,17 @@
 package com.gin.reservationinformationsystem.module.merchant.bo;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.gin.reservationinformationsystem.sys.params_validation.annotation.EffectiveValues;
 import com.gin.reservationinformationsystem.sys.request.IFilter;
+import com.gin.reservationinformationsystem.sys.utils.QueryWrapperUtils;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
+
+import static com.gin.reservationinformationsystem.module.merchant.entity.FeedBackMessagePo.STATUS_NOT_PROCESSED;
+import static com.gin.reservationinformationsystem.module.merchant.entity.FeedBackMessagePo.STATUS_PROCESSED;
 
 /**
  * @author bx002
@@ -15,10 +20,11 @@ import java.io.Serializable;
 @ApiModel("查询过滤条件")
 public class Filter4FeedBackMessagePo implements Serializable, IFilter {
     @ApiModelProperty("状态")
+    @EffectiveValues(prefix = "状态", values = {STATUS_NOT_PROCESSED, STATUS_PROCESSED}, nullable = true)
     String status;
 
     @Override
     public void handleQueryWrapper(QueryWrapper<?> queryWrapper) {
-        queryWrapper.eq("status",status);
+        QueryWrapperUtils.equalsIfNotEmpty(queryWrapper, "status", status);
     }
 }
